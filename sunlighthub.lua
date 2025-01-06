6d2eddc366ca7897dac6ab004e55ca474ed0ebce
alodozhynn <alodotocanaru@gmail.com>
2025-01-06 17:30:49 -0300

    Update sunlighthub.lua

--- sunlighthub.lua
+++ sunlighthub.lua
@@ -102,7 +102,7 @@ local Button = Tab:CreateButton({
     Callback = function()
         local args = {
             [1] = "CharacterSizeDown",
-            [2] = 3.5
+            [2] = 2
         }
         game:GetService("ReplicatedStorage").RE:FindFirstChild("1Clothe1s"):FireServer(unpack(args))
     end,
